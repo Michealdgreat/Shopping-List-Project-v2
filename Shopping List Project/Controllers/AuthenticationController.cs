e7da6b41113f327f4d42d744a3c1d8255859736c
MICHEAL SHODAMOLA <michealshodamola01@gmail.com>
2024-01-16 18:58:54 +0200

    An API that saves a shopping list, calculate expenses and gives a report on spending.

--- Shopping List Project/Controllers/AuthenticationController.cs
+++ Shopping List Project/Controllers/AuthenticationController.cs
@@ -23,45 +23,23 @@ public class AuthenticationController : ControllerBase
         _config = config;
     }
 
-    public record AuthenticationData(string? UserName, string? Password);
+    public record AuthenticationData(string? UserName, string? Password); //class with a parameter.
     public record UserData(int Id, string FirstName, string LastName, string UserName);
 
     [HttpPost("token")]
     [AllowAnonymous]
     public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
     {
-        var user = ValidateCredentials(data);
+        var user = ValidateCredentials(data); // compare username and password provided by user
 
         if (user == null)
         {
             return Unauthorized();
         }
 
-        string token = GenerateToken(user);
+        string token = GenerateToken(user); // UserData user // instance of UserData
 
         return Ok(token);
-    }
-
-    private string GenerateToken(UserData user)
-    {
-        var secretKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config.GetValue<string>("Authentication:Secretkey")));
-
-        var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
-
-        List<Claim> claims = new();
-        claims.Add(new(JwtRegisteredClaimNames.Sub, user.Id.ToString()));
-        claims.Add(new(JwtRegisteredClaimNames.UniqueName, user.UserName));
-        claims.Add(new(JwtRegisteredClaimNames.GivenName, user.FirstName));
-        claims.Add(new(JwtRegisteredClaimNames.FamilyName, user.LastName));
-
-
-        var token = new JwtSecurityToken(_config.GetValue<string>("Authentication:Issuer"), 
-                                                        _config.GetValue<string>("Authentication:Audience"), 
-                                                        claims, DateTime.UtcNow, DateTime.UtcNow.AddMinutes(60), 
-                                                        signingCredentials);
-
-        return new JwtSecurityTokenHandler().WriteToken(token);
-
     }
 
     private UserData? ValidateCredentials(AuthenticationData data)
@@ -84,9 +62,32 @@ public class AuthenticationController : ControllerBase
     {
         if (actual != null)
         {
-            if(actual == expected) return true;
+            if (actual == expected) return true;
         }
 
         return false;
+    }
+
+
+    private string GenerateToken(UserData user)
+    {
+        var secretKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config.GetValue<string>("Authentication:Secretkey")));
+
+        var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        List<Claim> claims = new();
+        claims.Add(new(JwtRegisteredClaimNames.Sub, user.Id.ToString()));
+        claims.Add(new(JwtRegisteredClaimNames.UniqueName, user.UserName));
+        claims.Add(new(JwtRegisteredClaimNames.GivenName, user.FirstName));
+        claims.Add(new(JwtRegisteredClaimNames.FamilyName, user.LastName));
+
+
+        var token = new JwtSecurityToken(_config.GetValue<string>("Authentication:Issuer"), 
+                                                        _config.GetValue<string>("Authentication:Audience"), 
+                                                        claims, DateTime.UtcNow, DateTime.UtcNow.AddMinutes(60), 
+                                                        signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+
     }
 }
